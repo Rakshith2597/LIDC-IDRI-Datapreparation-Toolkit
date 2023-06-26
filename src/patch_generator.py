0c72e17545d9e556a8aeae5e65ad91c98389d327
Sankarshanaa Sagaram <sanky.sagaram@gmail.com>
2023-06-26 17:36:35 +0530

    modularised path_generator.py file along with documentation

---
+++ src/patch_generator.py
@@ -0,0 +1,205 @@
+import numpy as np
+import os
+import cv2
+import json
+from tqdm import tqdm as tq
+
+class PatchExtractor:
+    """
+    A class for extracting patches from image volumes and corresponding masks.
+
+    Parameters:
+        data_path (str): The path to the data directory.
+
+    Attributes:
+        data_path (str): The path to the data directory.
+        imgpath (str): The path to the images directory.
+        consensus_maskpath (str): The path to the consensus masks directory.
+        intersection_maskpath (str): The path to the intersection masks directory.
+        union_maskpath (str): The path to the union masks directory.
+        savepath (str): The path to the patches directory.
+        file_list (list): A list of filenames in the images directory.
+        consensus_list (list): A list of filenames in the consensus masks directory.
+        union_list (list): A list of filenames in the union masks directory.
+        intersection_list (list): A list of filenames in the intersection masks directory.
+        series_id_list (list): A list of series IDs.
+        id_list_2 (list): A list of IDs extracted from filenames in the images directory.
+        series_id_new_list (list): A filtered list of series IDs that exist in both series_id_list and id_list_2.
+    """
+
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.imgpath = os.path.join(data_path, 'images')
+        self.consensus_maskpath = os.path.join(data_path, 'consensus_masks')
+        self.intersection_maskpath = os.path.join(data_path, 'intersection_masks')
+        self.union_maskpath = os.path.join(data_path, 'union_masks')
+        self.savepath = os.path.join(data_path, 'patches')
+        
+        if not os.path.exists(self.savepath):
+            os.makedirs(self.savepath)
+        
+        with open('/home/rakshith/MIRIAD_WP4/stage3_segmentation_classification/jsons/seriesuid_nodule_map.json') as f:
+            self.seriesuid_nod = json.load(f)
+        
+    def setup_file_paths(self):
+          """
+        Set up the file paths and create lists of filenames.
+
+        Returns:
+            None
+        """
+        self.file_list = os.listdir(self.imgpath)
+        self.consensus_list = os.listdir(self.consensus_maskpath)
+        self.union_list = os.listdir(self.union_maskpath)
+        self.intersection_list = os.listdir(self.intersection_maskpath)
+        
+        self.series_id_list = self.seriesuid_nod.keys()
+        self.id_list_2 = [id.split('.npy')[0] for id in self.file_list]
+        self.series_id_new_list = [id for id in self.series_id_list if id in self.id_list_2]
+        
+    def process_patches(self):
+        """
+        Process the image volumes and corresponding masks to extract patches.
+
+        Returns:
+            None
+        """
+        img_savepath = os.path.join(self.savepath, 'img')
+        mask_savepath = os.path.join(self.savepath, 'c_mask')
+        u_mask_savepath = os.path.join(self.savepath, 'u_mask')
+        i_mask_savepath = os.path.join(self.savepath, 'i_mask')
+        
+        if not os.path.exists(img_savepath):
+            os.makedirs(img_savepath)
+        
+        if not os.path.exists(mask_savepath):
+            os.makedirs(mask_savepath)
+        
+        if not os.path.exists(u_mask_savepath):
+            os.makedirs(u_mask_savepath)
+        
+        if not os.path.exists(i_mask_savepath):
+            os.makedirs(i_mask_savepath)
+        
+        for series_id in tq(self.series_id_new_list):
+            size = 64
+            index = 0
+            filename = series_id + '.npy'
+            img_vol = np.load(os.path.join(self.imgpath, filename))
+            mask_vol = np.load(os.path.join(self.consensus_maskpath, filename))
+            union_vol = np.load(os.path.join(self.union_maskpath, filename))
+            intersection_vol = np.load(os.path.join(self.intersection_maskpath, filename))
+            img_shape = img_vol.shape
+            seriesid = filename.split('.npy')[0]
+            missing_list = []
+            slice_list = self.seriesuid_nod[series_id].keys()
+
+            for sl in slice_list:
+                sl = int(sl)
+                mask = mask_vol[:, :, sl]
+                mask = mask.astype(np.uint8)
+
+                if np.any(mask):
+                    img = img_vol[:, :, sl]
+                    u_mask = union_vol[:, :, sl]
+                    u_mask = u_mask.astype(np.uint8)
+                    i_mask = intersection_vol[:, :, sl]
+                    i_mask = i_mask.astype(np.uint8)
+                    keys_in_json = self.seriesuid_nod[seriesid].keys()
+                    noduleno = self.seriesuid_nod[seriesid][str(sl)]
+                    _, th_mask = cv2.threshold(mask, 0.5, 1, 0, cv2.THRESH_BINARY)
+
+                    contours, hierarchy = cv2.findContours(th_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                    contours = sorted(contours, key=lambda x: cv2.contourArea(x))
+
+                    _, uth_mask = cv2.threshold(u_mask, 0.5, 1, 0, cv2.THRESH_BINARY)
+                    u_contours, u_hierarchy = cv2.findContours(uth_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                    u_contours = sorted(u_contours, key=lambda x: cv2.contourArea(x))
+
+                    _, ith_mask = cv2.threshold(i_mask, 0.5, 1, 0, cv2.THRESH_BINARY)
+                    i_contours, i_hierarchy = cv2.findContours(ith_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                    i_contours = sorted(i_contours, key=lambda x: cv2.contourArea(x))
+
+                    for cntr in contours:
+                        xr, yr, wr, hr = cv2.boundingRect(cntr)
+                        xc, yc = int(xr + wr / 2), int(yr + hr / 2)
+
+                        if int(yc - size / 2) < 0 or int(xc - size / 2) < 0:
+                            if int(yc - size / 2) < 0 and int(xc - size / 2) < 0:
+                                patch_img1 = img[0:size, 0:size].copy().astype(np.float16)
+                                patch_mask1 = mask[0:size, 0:size].copy().astype(np.float16)
+                                patch_mask2 = u_mask[0:size, 0:size].copy().astype(np.float16)
+                                patch_mask3 = i_mask[0:size, 0:size].copy().astype(np.float16)
+
+                            elif int(yc - size / 2) > 0 and int(xc - size / 2) < 0:
+                                patch_img1 = img[int(yc - size / 2):int(yc + size / 2), 0:size].copy().astype(np.float16)
+                                patch_mask1 = mask[int(yc - size / 2):int(yc + size / 2), 0:size].copy().astype(np.float16)
+                                patch_mask2 = u_mask[0:size, 0:size].copy().astype(np.float16)
+                                patch_mask3 = i_mask[0:size, 0:size].copy().astype(np.float16)
+
+                            elif int(yc - size / 2) < 0 and int(xc - size / 2) > 0:
+                                patch_img1 = img[0:size, int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+                                patch_mask1 = mask[0:size, int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+                                patch_mask2 = u_mask[0:size, 0:size].copy().astype(np.float16)
+                                patch_mask3 = i_mask[0:size, 0:size].copy().astype(np.float16)
+
+                        elif int(yc + size / 2) > 512 or int(xc + size / 2) > 512:
+                            if int(yc + size / 2) > 512 and int(xc + size / 2) > 512:
+                                m = yc + size - 512
+                                n = xc + size - 512
+                                patch_img1 = img[int(yc - m):512, int(xc - n):512].copy().astype(np.float16)
+                                patch_mask1 = mask[int(yc - m):512, int(xc - n):512].copy().astype(np.float16)
+                                patch_mask2 = u_mask[0:size, 0:size].copy().astype(np.float16)
+                                patch_mask3 = i_mask[0:size, 0:size].copy().astype(np.float16)
+
+                            elif int(yc + size / 2) > 512 and int(xc + size / 2) < 512:
+                                m = yc + size - 512
+                                patch_img1 = img[int(yc - m):512, int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+                                patch_mask1 = mask[int(yc - m):512, int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+                                patch_mask2 = u_mask[int(yc - m):512, int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+                                patch_mask3 = i_mask[int(yc - m):512, int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+
+                            elif int(yc + size / 2) < 512 and int(xc + size / 2) > 512:
+                                n = xc + size - 512
+                                patch_img1 = img[int(yc - size / 2):int(yc + size / 2), int(xc - n):512].copy().astype(np.float16)
+                                patch_mask1 = mask[int(yc - size / 2):int(yc + size / 2), int(xc - n):512].copy().astype(np.float16)
+                                patch_mask2 = u_mask[int(yc - size / 2):int(yc + size / 2), int(xc - n):512].copy().astype(np.float16)
+                                patch_mask3 = i_mask[int(yc - size / 2):int(yc + size / 2), int(xc - n):512].copy().astype(np.float16)
+
+                        elif int(yc - size / 2) >= 0 and int(yc + size / 2) <= 512:
+                            if int(xc - size / 2) >= 0 and int(xc + size / 2) <= 512:
+                                patch_img1 = img[int(yc - size / 2):int(yc + size / 2), int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+                                patch_mask1 = mask[int(yc - size / 2):int(yc + size / 2), int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+                                patch_mask2 = u_mask[int(yc - size / 2):int(yc + size / 2), int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+                                patch_mask3 = i_mask[int(yc - size / 2):int(yc + size / 2), int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+
+                        if np.shape(patch_img1) != (64, 64):
+                            print('shape', np.shape(patch_img1))
+                            print('cordinate of BBox', xr, yr, wr, hr)
+
+                        if not os.path.isdir(img_savepath):
+                            os.makedirs(img_savepath)
+
+                        if not os.path.isdir(mask_savepath):
+                            os.makedirs(mask_savepath)
+
+                        if not os.path.isdir(u_mask_savepath):
+                            os.makedirs(u_mask_savepath)
+
+                        if not os.path.isdir(i_mask_savepath):
+                            os.makedirs(i_mask_savepath)
+
+                        np.save(os.path.join(img_savepath, seriesid + '_' + str(index) + '.npy'), patch_img1)
+                        np.save(os.path.join(mask_savepath, seriesid + '_' + str(index) + '.npy'), patch_mask1)
+                        np.save(os.path.join(u_mask_savepath, seriesid + '_' + str(index) + '.npy'), patch_mask2)
+                        np.save(os.path.join(i_mask_savepath, seriesid + '_' + str(index) + '.npy'), patch_mask3)
+
+                        index += 1
+
+        print('Patches Extraction Completed!')
+
+
+#to execute:
+#extractor = PatchExtractor('/path/to/data')
+#extractor.setup_file_paths()
+#extractor.process_patches()
