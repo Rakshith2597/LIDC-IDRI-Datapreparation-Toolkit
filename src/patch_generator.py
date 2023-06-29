fe64fadfb0812b034042d8f871ddcba5416bd16e
Sankarshanaa Sagaram <sanky.sagaram@gmail.com>
2023-06-30 01:12:54 +0530

    modularised patchgenerator file with two classes

--- src/patch_generator.py
+++ src/patch_generator.py
@@ -3,6 +3,10 @@ import os
 import cv2
 import json
 from tqdm import tqdm as tq
+import pickle
+from skimage.util.shape import view_as_windows
+import random
+from collections import defaultdict
 
 class PatchExtractor:
     """
@@ -203,3 +207,181 @@ class PatchExtractor:
 #extractor = PatchExtractor('/path/to/data')
 #extractor.setup_file_paths()
 #extractor.process_patches()
+
+
+class ImagePatchSplitter:
+    def __init__(self, current_dir, target_location):
+        """
+        Initializes an instance of the ImagePatchSplitter class.
+
+        Parameters:
+        - current_dir (str): The current directory path.
+        - target_location (str): The target location directory path.
+        """
+        self.current_dir = current_dir
+        self.target_location = target_location
+        self.data_path = os.path.join(target_location, 'isbi_data2')
+        self.imgpath = os.path.join(self.data_path, 'images')
+        self.maskpath = os.path.join(self.data_path, 'masks')
+        self.savepath = self.data_path
+        self.img_dir = self.imgpath
+        self.mask_dir = self.maskpath
+        self.seriesuid_nod = {}
+
+    def load_seriesuid_nodule_map(self, filepath):
+        """
+        Loads the seriesuid_nodule_map from the specified filepath.
+
+        Parameters:
+        - filepath (str): The filepath of the seriesuid_nodule_map JSON file.
+        """
+        with open(filepath) as f:
+            self.seriesuid_nod = json.load(f)
+
+    def process_image_patches(self, size=64):
+        """
+        Processes image patches from the input images and masks.
+
+        Parameters:
+        - size (int): The size of the image patch. Default is 64.
+
+        Prints:
+        - shape: Shape of the image patch if it is not (64, 64).
+        - coordinate of patch: x, y coordinates of the patch.
+        - coordinate of BBox: x, y, width, height of the bounding box.
+
+        Saves:
+        - Patches: Image patches and corresponding masks as numpy arrays in the 'patches/img' and 'patches/mask' directories.
+        """
+        file_list = os.listdir(self.imgpath)
+        for idx, filename in enumerate(tq(file_list)):
+            index = 0
+            img_vol = np.load(os.path.join(self.img_dir, filename))
+            mask_vol = np.load(os.path.join(self.mask_dir, filename))
+            img_shape = img_vol.shape
+            seriesid = filename.split('.npy')[0]
+
+            for sl in range(img_shape[2]):
+                img = img_vol[:, :, sl]
+                mask = mask_vol[:, :, sl]
+                mask = mask.astype(np.uint8)
+
+                if np.any(mask):
+                    noduleno = self.seriesuid_nod[seriesid][str(sl)]
+                    _, th_mask = cv2.threshold(mask, 0.5, 1, 0, cv2.THRESH_BINARY)
+                    contours, hierarchy = cv2.findContours(th_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                    contours = sorted(contours, key=lambda x: cv2.contourArea(x))
+
+                    for cntr in contours:
+                        xr, yr, wr, hr = cv2.boundingRect(cntr)
+                        xc, yc = int(xr + wr / 2), int(yr + hr / 2)
+
+                        if int(yc - size / 2) < 0 or int(xc - size / 2) < 0:
+                            if int(yc - size / 2) < 0 and int(xc - size / 2) < 0:
+                                patch_img1 = img[0:size, 0:size].copy().astype(np.float16)
+                                patch_mask1 = mask[0:size, 0:size].copy().astype(np.float16)
+
+                            elif int(yc - size / 2) > 0 and int(xc - size / 2) < 0:
+                                patch_img1 = img[int(yc - size / 2):int(yc + size / 2), 0:size].copy().astype(np.float16)
+                                patch_mask1 = mask[int(yc - size / 2):int(yc + size / 2), 0:size].copy().astype(np.float16)
+
+                            elif int(yc - size / 2) < 0 and int(xc - size / 2) > 0:
+                                patch_img1 = img[0:size, int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+                                patch_mask1 = mask[0:size, int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+
+                        elif int(yc + size / 2) > 512 or int(xc + size / 2) > 512:
+                            if int(yc + size / 2) > 512 and int(xc + size / 2) > 512:
+                                m = yc + size - 512
+                                n = xc + size - 512
+                                patch_img1 = img[int(yc - m):512, int(xc - n):512].copy().astype(np.float16)
+                                patch_mask1 = mask[int(yc - m):512, int(xc - n):512].copy().astype(np.float16)
+
+                            elif int(yc + size / 2) > 512 and int(xc + size / 2) < 512:
+                                m = yc + size - 512
+                                patch_img1 = img[int(yc - m):512, int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+                                patch_mask1 = mask[int(yc - m):512, int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+
+                            elif int(yc + size / 2) < 512 and int(xc + size / 2) > 512:
+                                n = xc + size - 512
+                                patch_img1 = img[int(yc - size / 2):int(yc + size / 2), int(xc - n):512].copy().astype(np.float16)
+                                patch_mask1 = mask[int(yc - size / 2):int(yc + size / 2), int(xc - n):512].copy().astype(np.float16)
+
+                        elif (int(yc - size / 2) >= 0 and int(yc + size / 2) <= 512):
+                            if (int(xc - size / 2) >= 0 and int(xc + size / 2) <= 512):
+                                patch_img1 = img[int(yc - size / 2):int(yc + size / 2), int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+                                patch_mask1 = mask[int(yc - size / 2):int(yc + size / 2), int(xc - size / 2):int(xc + size / 2)].copy().astype(np.float16)
+
+                        if np.shape(patch_img1) != (64, 64):
+                            print('shape', np.shape(patch_img1))
+                            print('coordinate of patch', x, x + size, y, y + size)
+                            print('coordinate of BBox', xr, yr, wr, hr)
+
+                        img_savepath = self.savepath + '/patches/' + '/img/'
+                        mask_savepath = self.savepath + '/patches/' + '/mask/'
+                        if not os.path.isdir(img_savepath):
+                            os.makedirs(self.savepath + '/patches/' + '/img/')
+                            np.save(img_savepath + seriesid + '_' + str(sl) + '_' + str(noduleno) + '.npy', patch_img1)
+                        else:
+                            np.save(img_savepath + seriesid + '_' + str(sl) + '_' + str(noduleno) + '.npy', patch_img1)
+
+                        if not os.path.isdir(mask_savepath):
+                            os.makedirs(self.savepath + '/patches/' + '/mask/')
+                            np.save(mask_savepath + seriesid + '_' + str(sl) + '_' + str(noduleno) + '.npy', patch_mask1)
+                        else:
+                            np.save(mask_savepath + seriesid + '_' + str(sl) + '_' + str(noduleno) + '.npy', patch_mask1)
+
+    def split_data(self):
+        """
+        Split the data into train, validation, and test sets.
+
+        This method splits the image data into train, validation, and test sets based on a random sampling of series IDs. The split ratio is 70% for training, 20% for validation, and 10% for testing. The split information is saved in a JSON file named 'patch_split_new.json'.
+
+        Returns:
+            None
+        """
+        image_list = os.listdir(self.imgpath)
+        series_list = []
+        for file in image_list:
+            seriesid = file.split('_')[0]
+            if seriesid not in series_list:
+                series_list.append(seriesid)
+
+        test_list = random.sample(series_list, 70)
+        valid_list = []
+        train_list = []
+        count = 0
+        for file in series_list:
+            if file not in test_list:
+                valid_list.append(file)
+                count += 1
+
+            if count == 70:
+                break
+
+        for file in series_list:
+            if file not in test_list:
+                if file not in valid_list:
+                    train_list.append(file)
+
+        train_image_list = []
+        valid_image_list = []
+        test_image_list = []
+
+        for idx in train_list:
+            for file in image_list:
+                if idx in file:
+                    train_image_list.append(file)
+
+        for idx in valid_list:
+            for file in image_list:
+                if idx in file:
+                    valid_image_list.append(file)
+
+        for idx in test_list:
+            for file in image_list:
+                if idx in file:
+                    test_image_list.append(file)
+
+        patch_split = {'train_set': train_image_list, 'valid_set': valid_image_list, 'test_set': test_image_list}
+        with open('patch_split_new.json', 'w') as f:
+            json.dump(patch_split, f)
