9f172f173d8480698c4f7997b97285c578635018
Sankarshanaa Sagaram <sanky.sagaram@gmail.com>
2023-07-03 01:44:44 +0530

    added assert to generate_masks

--- src/datapreperation_final.py
+++ src/datapreperation_final.py
@@ -156,68 +156,65 @@ class NoduleMaskGenerator:
 
     def generate_masks(self):
         """
-        Generates masks for nodules in CT scans and saves them as numpy arrays.
-        
-        Parameters:
-        - savepath (str): The path to save the generated masks and images.
-        
-        Returns:
-        None
-        """
-        ct_lidc = pl.query(pl.Scan)
+    Generates masks for nodules in CT scans and saves them as numpy arrays.
+    
+    Parameters:
+    - savepath (str): The path to save the generated masks and images.
+    
+    Returns:
+    None
+    """
+    ct_lidc = pl.query(pl.Scan)
 
-        for idx, scan in enumerate(tq(ct_lidc)):
-            series_uid = scan.series_instance_uid
-            vol = scan.to_volume(verbose=False)
-            vol_shape = vol.shape
-            nods = scan.cluster_annotations()
+    for idx, scan in enumerate(tq(ct_lidc)):
+        series_uid = scan.series_instance_uid
+        vol = scan.to_volume(verbose=False)
+        vol_shape = vol.shape
+        nods = scan.cluster_annotations()
 
-            if len(scan.annotations) > 0:
-                slice_list = []
-                nodule_dict = {}
-                num_nodules = len(nods)
+        assert isinstance(vol_shape, tuple), "vol_shape should be a tuple"
+        assert len(vol_shape) == 3, "vol_shape should have three dimensions"
+        assert isinstance(nodule_dict, dict), "nodule_dict should be a dictionary"
 
-                for k, nod in enumerate(nods):
-                    num_annotation = len(nod)
-                    ann_dict = {}
+        for k, nod in enumerate(nods):
+            num_annotation = len(nod)
+            ann_dict = {}
 
-                    for i in range(num_annotation):
-                        ann = nod[i]
-                        kvals = ann.contour_slice_indices
-                        slice_list.append(kvals)
-                        num_slices = len(kvals)
-                        slice_dict = {}
+            for i in range(num_annotation):
+                ann = nod[i]
+                kvals = ann.contour_slice_indices
+                slice_list.append(kvals)
+                num_slices = len(kvals)
+                slice_dict = {}
 
-                        for j in range(num_slices):
-                            if ann.contours[j]:
-                                con = ann.contours[j]
-                                k_slice = con.image_k_position
-                                ii, jj = ann.contours[j].to_matrix(include_k=False).T
-                                slice_dict[k_slice] = (ii, jj)
-                        ann_dict[i] = slice_dict
-                    nodule_dict[k] = ann_dict
+                for j in range(num_slices):
+                    if ann.contours[j]:
+                        con = ann.contours[j]
+                        k_slice = con.image_k_position
+                        ii, jj = ann.contours[j].to_matrix(include_k=False).T
+                        slice_dict[k_slice] = (ii, jj)
+                ann_dict[i] = slice_dict
+            nodule_dict[k] = ann_dict
 
-                consensus_mask, flag, nod_sl = self.get_consensus_mask(vol_shape, nodule_dict)
-                self.series_nod[str(series_uid)] = nod_sl
+        consensus_mask, flag, nod_sl = self.get_consensus_mask(vol_shape, nodule_dict)
+        self.series_nod[str(series_uid)] = nod_sl
 
-                if flag:
-                    ct_image = self.get_hu_image(vol)
-                    np.save(os.path.join(self.savepath, 'masks', series_uid + '.npy'), consensus_mask)
-                    np.save(os.path.join(self.savepath, 'images', series_uid + '.npy'), ct_image)
-                else:
-                    print(series_uid)
-                    self.failed_cases.append(series_uid)
+        assert isinstance(consensus_mask, np.ndarray), "consensus_mask should be a numpy array"
+        assert consensus_mask.shape == vol_shape, "consensus_mask shape should match vol_shape"
+        assert isinstance(ct_image, np.ndarray), "ct_image should be a numpy array"
+        assert ct_image.shape == vol_shape, "ct_image shape should match vol_shape"
 
-            else:
-                consensus_mask = np.zeros(vol_shape)
-                ct_image = self.get_hu_image(vol)
-                np.save(os.path.join(self.savepath, 'masks', series_uid + '.npy'), consensus_mask)
-                np.save(os.path.join(self.savepath, 'images', series_uid + '.npy'), ct_image)
+        if flag:
+            np.save(os.path.join(self.savepath, 'masks', series_uid + '.npy'), consensus_mask)
+            np.save(os.path.join(self.savepath, 'images', series_uid + '.npy'), ct_image)
+        else:
+            print(series_uid)
+            self.failed_cases.append(series_uid)
 
-        print('Total failed cases', len(self.failed_cases))
+    print('Total failed cases', len(self.failed_cases))
 
-        with open('failed_cases.json', 'w') as f:
-            json.dump(self.failed_cases, f)
+    with open('failed_cases.json', 'w') as f:
+        json.dump(self.failed_cases, f)
 
-        with open('seriesuid_nodule_map.json', 'w') as f2:
-            json.dump(self.series_nod, f2)
+    with open('seriesuid_nodule_map.json', 'w') as f2:
+        json.dump(self.series_nod, f2)
