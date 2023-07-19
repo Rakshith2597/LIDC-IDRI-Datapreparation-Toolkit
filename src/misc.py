7e9c4ba4473cd27e3db49c958a2a6e522ff1263c
Sankarshanaa Sagaram <sanky.sagaram@gmail.com>
2023-07-19 22:52:19 +0530

    new function added

--- src/misc.py
+++ src/misc.py
@@ -1,1 +1,23 @@
 # file to visualize images and json load function
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def data_clean_hist(img_dir):
+    """
+    Plot the histogram of an image file.
+
+    Args:
+        img_dir (str): Directory path of the image file.
+
+    Returns:
+        None
+    """
+    img = np.load(img_dir)
+    plt.hist(img)
+    plt.save()
+
+
+
