196d479c5c7c2a231c41d233873d4749d201acb8
Sankarshanaa Sagaram <sanky.sagaram@gmail.com>
2023-08-22 11:51:29 +0530

    added function data_diff_plot_uncertain_ratio

--- src/misc.py
+++ src/misc.py
@@ -64,3 +64,53 @@ def data_difficulty_hist(data, color='#EA5455', bgcolor='#F9F5EB', title='', xla
 
 # fig = plot_histogram(df, title='My Histogram', ylabel='Number') 
 # fig.show()
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def data_diff_plot_uncertain_ratio(ratio, img_path, union_path, intersection_path, consensus_path):
+    """
+    Plot images based on uncertain ratio.
+    
+    Args:
+        ratio (float): The uncertain ratio value
+        img_path (str): Path to image folders
+        union_path (str): Path to union image folders 
+        intersection_path (str): Path to intersection image folders
+        consensus_path (str): Path to consensus image folders
+        
+    Returns:
+        None
+        
+    Raises:
+        ImportError: If matplotlib is not installed
+    """
+
+    if plt is None:
+        raise ImportError("Matplotlib is not installed")
+
+    print(f'Uncertain Ratio: {ratio}')
+    
+    plt.subplot(141)
+    plt.title('Union')
+    im1 = np.load(img_path + '/' + ratio_dict[ratio][0]).astype(np.float)
+    plt.imshow(im1, cmap='gray')
+    
+    plt.subplot(142) 
+    plt.title('Union')
+    im1 = np.load(union_path + '/' + ratio_dict[ratio][0]).astype(np.float)
+    plt.imshow(im1, cmap='gray')
+
+    plt.subplot(143)
+    plt.title('Intersection')
+    im2 = np.load(intersection_path + '/' + ratio_dict[ratio][0]).astype(np.float)
+    plt.imshow(im2, cmap='gray')
+
+    plt.subplot(144)
+    plt.title('Consensus')
+    im2 = np.load(consensus_path + '/' + ratio_dict[ratio][0]).astype(np.float)
+    plt.imshow(im2, cmap='gray')
+
+# example usage: 
+# ratio = 0.9
+# plot_images(ratio, img_path, union_path, intersection_path, consensus_path)
